df4600f4a7213256f491465219cf6db1d0633e34
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-27 15:06:34 +0900

    feat: Member Entity 구현

---
+++ src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,30 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}
