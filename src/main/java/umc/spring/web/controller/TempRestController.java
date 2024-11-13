4d51fb541c9ebd313d80f97cbf47557d2e93e577
isumin <leesumin0526@gmail.com>
2024-11-13 17:14:09 +0900

    [practice/#7-1] Controller 완성

--- src/main/java/umc/spring/web/controller/TempRestController.java
+++ src/main/java/umc/spring/web/controller/TempRestController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.ApiPayload.ApiResponse;
 import umc.spring.converter.TempConverter;
+import umc.spring.service.TempService.TempQueryService;
 import umc.spring.web.dto.TempResponse;
 
 @RestController
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
+    private final TempQueryService tempQueryService;
 
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
@@ -21,6 +23,7 @@ public class TempRestController {
 
     @GetMapping("/exception")
     public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
-        return null;
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExcetionDTO(flag));
     }
 }
