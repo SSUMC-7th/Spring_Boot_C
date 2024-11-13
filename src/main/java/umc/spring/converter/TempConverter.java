61b5a54f436967d7871550cb2ffe7d6781ab2dd0
isumin <leesumin0526@gmail.com>
2024-11-13 17:12:06 +0900

    [practice/#7-1] Service 작성

--- src/main/java/umc/spring/converter/TempConverter.java
+++ src/main/java/umc/spring/converter/TempConverter.java
@@ -8,4 +8,10 @@ public class TempConverter {
                 .testString("This is Test!")
                 .build();
     }
+
+    public static TempResponse.TempExceptionDTO toTempExcetionDTO(Integer flag){
+        return TempResponse.TempExceptionDTO.builder()
+                .flag(flag)
+                .build();
+    }
 }
--- src/main/java/umc/spring/web/controller/TempRestController.java
+++ src/main/java/umc/spring/web/controller/TempRestController.java
@@ -3,6 +3,7 @@ package umc.spring.web.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.ApiPayload.ApiResponse;
 import umc.spring.converter.TempConverter;
@@ -12,8 +13,14 @@ import umc.spring.web.dto.TempResponse;
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
+
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
+        return null;
     }
 }
