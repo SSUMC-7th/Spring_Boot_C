e1698d29599e7fda72a242b1bec6ce0ab65577be
isumin <leesumin0526@gmail.com>
2024-11-17 21:16:34 +0900

    [practice/#8] 기능 구현 완료

--- src/main/java/umc/spring/domain/enums/Gender.java
+++ src/main/java/umc/spring/domain/enums/Gender.java
@@ -1,5 +1,5 @@
 package umc.spring.domain.enums;
 
 public enum Gender {
-    ACTIVE, INACTIVE
+    MALE, FEMALE, NONE
 }
--- src/main/java/umc/spring/domain/mapping/MemberPrefer.java
+++ src/main/java/umc/spring/domain/mapping/MemberPrefer.java
@@ -12,6 +12,7 @@ import umc.spring.domain.common.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class MemberPrefer extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,7 +21,18 @@ public class MemberPrefer extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
-    private FoodCategory foodCategory;*/
+    private FoodCategory foodCategory;
+
+    public void setMember(Member member){
+        if(this.member != null)
+            member.getMemberPreferList().remove(this);
+        this.member = member;
+        member.getMemberPreferList().add(this);
+    }
+
+    public void setFoodCategory(FoodCategory foodCategory){
+        this.foodCategory = foodCategory;
+    }
 }
