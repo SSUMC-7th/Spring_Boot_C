d5dba4281be6c031c617c9f3ed9f8c110f9bbfa1
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-18 17:52:54 +0900

    practice(MemberCommandServiceImpl): 클래스 생성

---
+++ src/main/java/umc/study/service/MemberService/MemberCommandServiceImpl.java
@@ -0,0 +1,20 @@
+package umc.study.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.study.domain.Member;
+import umc.study.repository.MemberRepository.MemberRepository;
+import umc.study.web.dto.MemberRequestDTO;
+
+@Service
+@RequiredArgsConstructor
+public class MemberCommandServiceImpl implements MemberCommandService{
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member joinMember(MemberRequestDTO.JoinDto request) {
+
+        return null;
+    }
+}
