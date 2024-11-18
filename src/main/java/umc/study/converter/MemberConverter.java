c3df72a243e055c4f206085aca3d6fd7c17f1795
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-18 17:38:59 +0900

    practice(MemberConverter): 클래스 생성

---
+++ src/main/java/umc/study/converter/MemberConverter.java
@@ -0,0 +1,16 @@
+package umc.study.converter;
+
+import umc.study.domain.Member;
+import umc.study.web.dto.MemberResponseDTO;
+
+import java.time.LocalDateTime;
+
+public class MemberConverter {
+
+    public static MemberResponseDTO.JoinResultDTO toJoinResultDTO(Member member){
+        return MemberResponseDTO.JoinResultDTO.builder()
+                .memberId(member.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
