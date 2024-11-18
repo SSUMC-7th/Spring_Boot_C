8204d7b243e255847a5d5b8c2178ed641b95ef14
pwyi-cupi <pwyicupi@gmail.com>
2024-11-18 21:45:45 +0900

    practice : Swagger 세팅

--- src/main/java/umc/spring/service/MemberService/MemberQueryService.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.service.MemberService;
-
-import umc.spring.domain.Member;
-
-import java.util.List;
-
-public interface MemberQueryService{
-    List<Member> findMember(Long memberId);
-}
