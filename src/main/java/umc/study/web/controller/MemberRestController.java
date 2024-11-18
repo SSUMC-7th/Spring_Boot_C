26c6f6111612c1d027f2cdd755832cb040a5c80c
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-18 17:44:29 +0900

    practice(MemberRestController): 클래스 생성

---
+++ src/main/java/umc/study/web/controller/MemberRestController.java
@@ -0,0 +1,24 @@
+package umc.study.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.study.apiPayLoad.ApiResponse;
+import umc.study.service.MemberService.MemberCommandService;
+import umc.study.web.dto.MemberRequestDTO;
+import umc.study.web.dto.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+        return null;
+    }
+}
