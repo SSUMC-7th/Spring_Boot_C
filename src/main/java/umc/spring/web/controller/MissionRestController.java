c9e0b556279e54d83311cbd427e0f7123ec9546c
isumin <leesumin0526@gmail.com>
2024-11-19 15:39:40 +0900

    [mission/#8-2] Controller 생성

---
+++ src/main/java/umc/spring/web/controller/MissionRestController.java
@@ -0,0 +1,23 @@
+package umc.spring.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.ApiPayload.ApiResponse;
+import umc.spring.web.dto.MissionRequestDTO;
+import umc.spring.web.dto.MissionResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/missions")
+public class MissionRestController {
+    private final MissionCommandService missionCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MissionResponseDTO.MissionResultDTO> join(@RequestBody @Valid MissionRequestDTO.MissionJoinDto request){
+        return null;
+    }
+}
