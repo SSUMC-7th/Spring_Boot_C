285e315a9f3d206c87e9b814e51fc7f5efc73b9d
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 15:27:28 +0900

    practice(ReviewConverter): toReview 매서드 구현

--- src/main/java/umc/study/converter/review/ReviewConverter.java
+++ src/main/java/umc/study/converter/review/ReviewConverter.java
@@ -1,6 +1,9 @@
 package umc.study.converter.review;
 
+import umc.study.domain.Member;
 import umc.study.domain.Review;
+import umc.study.domain.Store;
+import umc.study.web.dto.ReviewRequestDTO;
 import umc.study.web.dto.ReviewResponseDTO;
 
 import java.time.LocalDateTime;
@@ -11,6 +14,15 @@ public class ReviewConverter {
         return ReviewResponseDTO.reviewResultDTO.builder()
                 .reviewId(review.getId())
                 .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Review toReview(ReviewRequestDTO.writeReviewDTO request, Member writerMember, Store writtenStore){
+
+        return Review.builder()
+                .member(writerMember)
+                .store(writtenStore)
+                .body(request.getReviewContent())
                 .build();
     }
 }
