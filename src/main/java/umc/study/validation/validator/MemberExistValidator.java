7f197631ff86dcac1af22b95e730e61328893cb3
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 18:21:56 +0900

    practice(MemberExistValidator): 구현 완료

---
+++ src/main/java/umc/study/validation/validator/MemberExistValidator.java
@@ -0,0 +1,39 @@
+package umc.study.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.study.apiPayLoad.code.status.ErrorStatus;
+import umc.study.repository.FoodCategoryRepository.FoodCategoryRepository;
+import umc.study.repository.MemberRepository.MemberRepository;
+import umc.study.validation.annotation.ExistCategories;
+import umc.study.validation.annotation.ExistMembers;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class MemberExistValidator implements ConstraintValidator<ExistMembers, List<Long>> {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void initialize(ExistMembers constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> memberRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.MEMBER_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
+    }
+}
