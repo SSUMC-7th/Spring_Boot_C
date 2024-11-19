8a20824fe48e947e269263e1fce87b5bc6f70377
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 13:36:47 +0900

    practice(StoreConverter): toJoinResultDTO 구현

---
+++ src/main/java/umc/study/converter/StoreConverter.java
@@ -0,0 +1,16 @@
+package umc.study.converter;
+
+import umc.study.domain.Store;
+import umc.study.web.dto.store.StoreResponseDTO;
+
+import java.time.LocalDateTime;
+
+public class StoreConverter {
+
+    public static StoreResponseDTO.JoinResultDTO toJoinResultDTO(Store store){
+        return StoreResponseDTO.JoinResultDTO.builder()
+                .storeId(store.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
