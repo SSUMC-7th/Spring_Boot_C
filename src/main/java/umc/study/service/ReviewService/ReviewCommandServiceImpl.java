42ceed5436f2933bd33cf1fed63e67633fe0f16e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 15:39:52 +0900

    practice(ReviewCommandServiceImpl): 구현 완료

--- src/main/java/umc/study/service/ReviewService/ReviewCommandServiceImpl.java
+++ src/main/java/umc/study/service/ReviewService/ReviewCommandServiceImpl.java
@@ -2,8 +2,17 @@ package umc.study.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.study.apiPayLoad.code.status.ErrorStatus;
+import umc.study.apiPayLoad.exception.handler.MemberHandler;
+import umc.study.apiPayLoad.exception.handler.StoreHandler;
+import umc.study.converter.review.ReviewConverter;
+import umc.study.domain.Member;
 import umc.study.domain.Review;
+import umc.study.domain.Store;
+import umc.study.repository.MemberRepository.MemberRepository;
 import umc.study.repository.ReviewRepository.ReviewRepository;
+import umc.study.repository.StoreRepository.StoreRepository;
 import umc.study.web.dto.ReviewRequestDTO;
 
 @Service
@@ -11,10 +20,15 @@ import umc.study.web.dto.ReviewRequestDTO;
 public class ReviewCommandServiceImpl implements ReviewCommandService{
 
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final StoreRepository storeRepository;
 
     @Override
+    @Transactional
     public Review writeReview(ReviewRequestDTO.writeReviewDTO request) {
-
-        return null;
+        Member member = memberRepository.findById(request.getMemberId()).orElseThrow(()-> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+        Store store = storeRepository.findById(request.getStoreId()).orElseThrow(()-> new StoreHandler(ErrorStatus.STORE_NOT_FOUND));
+        Review review = ReviewConverter.toReview(request, member, store);
+        return reviewRepository.save(review);
     }
 }
