d9ea312265a93b296cb651cb23ee8eac7a620d92
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 16:38:04 +0900

    practice(MyMissionCommandService): 인터페이스 구현

---
+++ src/main/java/umc/study/service/MissionService/MyMissionCommandService.java
@@ -0,0 +1,10 @@
+package umc.study.service.MissionService;
+
+import umc.study.domain.MemberMission;
+import umc.study.web.dto.MyMissionRequestDTO;
+
+import java.util.List;
+
+public interface MyMissionCommandService {
+    List<MemberMission> makeMyMission(MyMissionRequestDTO request);
+}
