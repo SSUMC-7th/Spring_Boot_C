dbae344463777177e95106f41d35cb147cd9f4d5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 12:25:21 +0900

    practice: Member의 age default 해제

--- src/main/java/umc/study/domain/Member.java
+++ src/main/java/umc/study/domain/Member.java
@@ -36,7 +36,7 @@ public class Member extends BaseEntity {
     @Column(nullable = false, columnDefinition = "varchar(10)")
     private Gender gender;
 
-    @Column(nullable = false)
+    @Column
     private Integer age;
 
     @Column(nullable = false, length = 40)
