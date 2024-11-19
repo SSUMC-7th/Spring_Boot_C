2ebccbccbdc7dcd072e39aac4b223d85ffd73f2a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 17:04:46 +0900

    practice(MissionRestController): 매서드 구현 완료

--- src/main/java/umc/study/web/controller/MissionRestController.java
+++ src/main/java/umc/study/web/controller/MissionRestController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.study.apiPayLoad.ApiResponse;
+import umc.study.converter.MemberMissionConverter;
+import umc.study.domain.MemberMission;
 import umc.study.service.MissionService.MyMissionCommandService;
 import umc.study.web.dto.MyMissionRequestDTO;
 import umc.study.web.dto.MyMissionResponseDTO;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class MissionRestController {
 
     @PostMapping("/my_mission")
     public ApiResponse<MyMissionResponseDTO.challengeResultDTO> join(@RequestBody @Valid MyMissionRequestDTO.challengeDTO request){
-        return null;
+        List<MemberMission> memberMissionList = myMissionCommandService.makeMyMission(request);
+        return ApiResponse.onSuccess(MemberMissionConverter.toMyMissionResponseDTO(memberMissionList));
     }
 }
