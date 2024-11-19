9b32e56795ce684dcf5903e10c84ebcd00e2eab4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 18:15:31 +0900

    practice(StoreRequestDTO): 클래스 구현

--- src/main/java/umc/study/web/dto/store/StoreRequestDTO.java
+++ src/main/java/umc/study/web/dto/store/StoreRequestDTO.java
@@ -2,13 +2,14 @@ package umc.study.web.dto.store;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import umc.study.validation.annotation.ExistRegions;
 
 public class StoreRequestDTO {
     @Getter
     public static class JoinDTO{
         @NotBlank
         String name;
-        @NotBlank
+        @ExistRegions
         Long regionId;
         @NotBlank
         String address;
