d9b0450e9d490065ebb9e9baf114097c739bdacd
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 16:02:11 +0900

    practice(ReviewRequestDTO): 벨리데이션 어노테이션 추가

--- src/main/java/umc/study/web/dto/ReviewRequestDTO.java
+++ src/main/java/umc/study/web/dto/ReviewRequestDTO.java
@@ -1,14 +1,19 @@
 package umc.study.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class ReviewRequestDTO {
 
     @Getter
     public static class writeReviewDTO{
+        @NotBlank
         Long memberId;
+        @NotBlank
         Long storeId;
+        @NotBlank
         Float score;
+        @NotBlank
         String reviewContent;
     }
 }
