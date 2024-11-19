d095b177a0a35f5a68b30dea0d8185506c8a2685
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 16:42:22 +0900

    practice(MyMissionCommandService): 틀린 부분 수정

--- src/main/java/umc/study/service/MissionService/MyMissionCommandService.java
+++ src/main/java/umc/study/service/MissionService/MyMissionCommandService.java
@@ -6,5 +6,5 @@ import umc.study.web.dto.MyMissionRequestDTO;
 import java.util.List;
 
 public interface MyMissionCommandService {
-    List<MemberMission> makeMyMission(MyMissionRequestDTO request);
+    List<MemberMission> makeMyMission(MyMissionRequestDTO.challengeDTO request);
 }
