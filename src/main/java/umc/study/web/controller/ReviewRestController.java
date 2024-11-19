96d20ab140a5d6046ee559ebb572c7dd3714cbe4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 15:24:25 +0900

    practice(ReviewRestController):클래스 구현

---
+++ src/main/java/umc/study/web/controller/ReviewRestController.java
@@ -0,0 +1,25 @@
+package umc.study.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.study.apiPayLoad.ApiResponse;
+import umc.study.service.ReviewService.ReviewCommandService;
+import umc.study.web.dto.ReviewRequestDTO;
+import umc.study.web.dto.ReviewResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
+public class ReviewRestController {
+
+    private final ReviewCommandService reviewCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<ReviewResponseDTO.reviewResultDTO> join(@RequestBody @Valid ReviewRequestDTO.writeReviewDTO request){
+        return null;
+    }
+}
