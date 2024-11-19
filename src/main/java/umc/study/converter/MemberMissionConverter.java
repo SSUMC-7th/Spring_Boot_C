b39e91d7708685712842e106bc52f46478a6991a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 17:14:32 +0900

    practice(MemberMissionConverter): toMyMissionResponseDTO 매서드 구현 완료

--- src/main/java/umc/study/converter/MemberMissionConverter.java
+++ src/main/java/umc/study/converter/MemberMissionConverter.java
@@ -1,7 +1,9 @@
 package umc.study.converter;
 
 import umc.study.domain.*;
+import umc.study.web.dto.MyMissionResponseDTO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,5 +17,16 @@ public class MemberMissionConverter {
                                 .mission(mission)
                                 .build()
                 ).collect(Collectors.toList());
+    }
+
+    public static MyMissionResponseDTO.challengeResultDTO toMyMissionResponseDTO(List<MemberMission> memberMissions){
+        List<Long> memberMissionId = memberMissions.stream()
+                .map(MemberMission::getId)
+                .toList();
+
+        return MyMissionResponseDTO.challengeResultDTO.builder()
+                .createdAt(LocalDateTime.now())
+                .createdMyMissions(memberMissionId)
+                .build();
     }
 }
