4862c6dea9aac65301720966aabc37d0ed2e7115
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 19:12:01 +0900

    practice: 커스텀 노테이션에 따른 벨리데이터 구현

---
+++ src/main/java/umc/study/validation/validator/MissionExistValidator.java
@@ -0,0 +1,37 @@
+package umc.study.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.study.apiPayLoad.code.status.ErrorStatus;
+import umc.study.validation.annotation.ExistMembers;
+import umc.study.validation.annotation.ExistMissions;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class MissionExistValidator implements ConstraintValidator<ExistMissions, List<Long>> {
+
+    private final MissionRepositoryService missionRepositoryService;
+
+    @Override
+    public void initialize(ExistMissions constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> missionRepositoryService.isInMissionRepository(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.MISSION_NOT_EXIST.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
+    }
+}
