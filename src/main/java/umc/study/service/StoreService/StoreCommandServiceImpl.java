386cb65589ad4de5a8a27a7af50ffd8bb8ebf2b7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 14:28:46 +0900

    practice(StoreCommandServiceImpl): 에러 핸들링 추가

--- src/main/java/umc/study/service/StoreService/StoreCommandServiceImpl.java
+++ src/main/java/umc/study/service/StoreService/StoreCommandServiceImpl.java
@@ -2,11 +2,18 @@ package umc.study.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.study.apiPayLoad.code.status.ErrorStatus;
+import umc.study.apiPayLoad.exception.handler.RegionHandler;
 import umc.study.converter.StoreConverter;
+import umc.study.domain.Member;
+import umc.study.domain.Region;
 import umc.study.domain.Store;
 import umc.study.repository.RegionRepository.RegionRepository;
 import umc.study.repository.StoreRepository.StoreRepository;
 import umc.study.web.dto.store.StoreRequestDTO;
+
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +26,7 @@ public class StoreCommandServiceImpl implements StoreCommandService{
     public Store joinStore(StoreRequestDTO.JoinDTO request) {
 
         Long regionId = request.getRegionId();
+        Region storeRegion = regionRepository.findById(regionId).orElseThrow(()->new RegionHandler(ErrorStatus.REGION_NOT_FOUND));
         Store store = StoreConverter.toStore(request,regionId);
         return null;
     }
