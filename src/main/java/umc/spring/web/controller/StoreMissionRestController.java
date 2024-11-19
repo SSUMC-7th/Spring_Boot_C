3bcb1c73801fde25238b52eb7546fc9e9ba4ef78
Minhyuk Song <poiuy010724@naver.com>
2024-11-19 21:55:39 +0900

    구현 완료

--- src/main/java/umc/spring/web/controller/StoreMissionRestController.java
+++ src/main/java/umc/spring/web/controller/StoreMissionRestController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayoad.ApiResponse;
+import umc.spring.converter.StoreMissionConverter;
 import umc.spring.domain.Mission;
+import umc.spring.service.StoreMissionService.StoreMissionCommandService;
+import umc.spring.web.dto.StoreMissionRequestDTO;
 import umc.spring.web.dto.StoreMissionResponseDTO;
 
 @RestController
@@ -15,10 +18,11 @@ import umc.spring.web.dto.StoreMissionResponseDTO;
 @RequestMapping("/missions")
 public class StoreMissionRestController {
 
-//    private final StoreMissionCommandService storeMissionCommandService;
+    private final StoreMissionCommandService storeMissionCommandService;
 
     @PostMapping("/stores")
-    public ApiResponse<StoreMissionResponseDTO.StoreMissionJoinResultDTO> addStoreMission(@RequestBody @Valid Mission mission) {
-        return null;
+    public ApiResponse<StoreMissionResponseDTO.StoreMissionJoinResultDTO> addStoreMission(@RequestBody @Valid StoreMissionRequestDTO.StoreMissionJoinDTO request) {
+        Mission mission = storeMissionCommandService.joinMission(request);
+        return ApiResponse.onSuccess(StoreMissionConverter.toStoreMissionJoinResultDTO(mission));
     }
 }
