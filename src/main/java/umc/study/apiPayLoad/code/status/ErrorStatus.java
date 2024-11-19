5499c750a35e4ac80f106be62b7d01b8f67a08a4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 16:49:03 +0900

    practice(ErrorStatus): 미션관련 에러 추가

--- src/main/java/umc/study/apiPayLoad/code/status/ErrorStatus.java
+++ src/main/java/umc/study/apiPayLoad/code/status/ErrorStatus.java
@@ -24,6 +24,9 @@ public enum ErrorStatus implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
     NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
 
+    //미션 관련 에러
+    MISSION_NOT_EXIST(HttpStatus.BAD_REQUEST,"MISSION4001","미션이 존재하지 않습니다"),
+
     // 예시,,,
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
     FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND,"FOOD_CATERGORY4001","음식 카테고리가 없습니다.");
