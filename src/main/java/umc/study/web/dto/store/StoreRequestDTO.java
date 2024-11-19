ed21540e24000a29a73ad279b3439ed24ad8be17
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 14:49:17 +0900

    practice(StoreRequestDTO): Constraint 추가

--- src/main/java/umc/study/web/dto/store/StoreRequestDTO.java
+++ src/main/java/umc/study/web/dto/store/StoreRequestDTO.java
@@ -1,12 +1,16 @@
 package umc.study.web.dto.store;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class StoreRequestDTO {
     @Getter
     public static class JoinDTO{
+        @NotBlank
         String name;
+        @NotBlank
         Long regionId;
+        @NotBlank
         String address;
     }
 }
