a3b82c15666cd1a87fa2282bf5c37ae34b8938a7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 13:40:57 +0900

    practice(StoreRestController): join 매서드 구현

---
+++ src/main/java/umc/study/web/controller/StoreRestController.java
@@ -0,0 +1,24 @@
+package umc.study.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.study.apiPayLoad.ApiResponse;
+import umc.study.web.dto.store.StoreRequestDTO;
+import umc.study.web.dto.store.StoreResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreRestController {
+
+    private final StoreCommandService storeCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<StoreResponseDTO.JoinResultDTO> join(@RequestBody @Valid StoreRequestDTO.JoinDTO request){
+        return null;
+    }
+}
