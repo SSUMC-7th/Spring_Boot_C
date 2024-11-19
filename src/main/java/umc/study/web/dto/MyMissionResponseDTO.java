75c957765a5ce7473f2e35b692ac179f0877cd04
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-19 16:14:53 +0900

    practice(MyMissionResponseDTO): 클래스 구현

---
+++ src/main/java/umc/study/web/dto/MyMissionResponseDTO.java
@@ -0,0 +1,10 @@
+package umc.study.web.dto;
+
+import java.time.LocalDateTime;
+
+public class MyMissionResponseDTO {
+    public static class challengeResultDTO{
+        Long memberId;
+        LocalDateTime createdAt;
+    }
+}
