b3a91ef5aa2edc137be638220dcc4db6fa4046a6
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-24 23:29:32 +0900

    practice(ReviewRequestDTO): 내가 작성한 리뷰 관련 DTO 생성

--- src/main/java/umc/study/web/dto/ReviewRequestDTO.java
+++ src/main/java/umc/study/web/dto/ReviewRequestDTO.java
@@ -1,9 +1,15 @@
 package umc.study.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import umc.study.validation.annotation.ExistMembers;
 import umc.study.validation.annotation.ExistStores;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class ReviewRequestDTO {
 
@@ -17,5 +23,29 @@ public class ReviewRequestDTO {
         Float score;
         @NotBlank
         String reviewContent;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewPreviewListDTO {
+        List<MyReviewPreviewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewPreviewDTO {
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
