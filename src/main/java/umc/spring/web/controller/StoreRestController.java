3c0f3f45d8b0333f3bc3ad163545054f2bb8fd91
Minhyuk Song <poiuy010724@naver.com>
2024-11-26 00:05:02 +0900

    feat : 특정 가게 미션 목록 조회 구현 완료
    controller 쪽에서 storeQueryService 객체 선언 실수가 있었다.
    수정 후 주가적인 파라미터 설명을 달았다.

--- src/main/java/umc/spring/web/controller/StoreRestController.java
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -24,10 +24,10 @@ import umc.spring.web.dto.StoreResponseDTO;
 @RequestMapping("/stores")
 public class StoreRestController {
 
-    StoreQueryService storeQueryService;
+    private final StoreQueryService storeQueryService;
 
     @GetMapping("/{storeId}/missions")
-    @Operation(summary = "특정 가게의 미션 목록 조회 API",description = "특정 가게의 미션들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @Operation(summary = "<- 특정 가게의 미션 목록 조회 API",description = "특정 가게의 미션들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
@@ -35,10 +35,11 @@ public class StoreRestController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @Parameters({
-            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!")
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "1번부터 시작하는 조회 페이지 번호입니다.")
     })
     public ApiResponse<StoreResponseDTO.StoreMissionListDTO> getMissionList(@ExistStore @PathVariable(name = "storeId") Long storeId, @IsPositive @RequestParam(name = "page") Integer page){
-        Page<Mission> missionList = storeQueryService.getMissionList(storeId,page);
+        Page<Mission> missionList = storeQueryService.getMissionList(storeId,page-1);
         return ApiResponse.onSuccess(StoreConverter.storeMissionListDTO(missionList));
     }
 }
