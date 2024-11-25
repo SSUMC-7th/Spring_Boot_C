2bdb8d421be0d1ac1a377fc686b789cfd7ace466
Minhyuk Song <poiuy010724@naver.com>
2024-11-25 18:54:26 +0900

    DTO, controller, converter 뼈대 완료

---
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -0,0 +1,17 @@
+package umc.spring.converter;
+
+import umc.spring.domain.Review;
+import umc.spring.web.dto.StoreResponseDTO;
+
+import java.util.List;
+
+public class StoreConverter {
+
+    public static StoreResponseDTO.ReviewPrivewDTO reviewPrivewDTO(Review review) {
+        return null;
+    }
+
+    public static StoreResponseDTO.ReviewPreviewListDTO reviewPreviewListDTO(List<Review> reviewList) {
+        return null;
+    }
+}
