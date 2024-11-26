da5dd4b5d876cf5cea6d959a39baca0d7295853f
isumin <leesumin0526@gmail.com>
2024-11-26 13:16:47 +0900

    [practice/#9] DTO 작성

--- src/main/java/umc/spring/web/controller/ReviewRestController.java
+++ src/main/java/umc/spring/web/controller/ReviewRestController.java
@@ -2,16 +2,15 @@ package umc.spring.web.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.ApiPayload.ApiResponse;
 import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Review;
 import umc.spring.service.ReviewService.ReviewCommandService;
+import umc.spring.validation.annotation.ExistStores;
 import umc.spring.web.dto.ReviewRequestDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
+import umc.spring.web.dto.StoreResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +22,10 @@ public class ReviewRestController {
     public ApiResponse<ReviewResponseDTO.ReviewJoinResultDTO> join(@RequestBody @Valid ReviewRequestDTO.ReviewJoinDto request) {
         Review review = reviewCommandService.joinReview(request);
         return ApiResponse.onSuccess(ReviewConverter.toJoinResultDTO(review));
+    }
+
+    @GetMapping("/{storeId}/reviews")
+    public ApiResponse<StoreResponseDTO.ReviewPreviewListDTO> getReviewList(@ExistStores @PathVariable(name = "storeId") Long storeId){
+        return null;
     }
 }
