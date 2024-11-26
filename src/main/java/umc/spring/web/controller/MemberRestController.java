b7dc21a35cc44c3d46bf9a72c04e2ce728d5110d
isumin <leesumin0526@gmail.com>
2024-11-26 17:59:18 +0900

    [practice/#9-getMyReviews] Controller 구현

--- src/main/java/umc/spring/web/controller/MemberRestController.java
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,5 +1,11 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +31,18 @@ public class MemberRestController {
     }
 
     @GetMapping("/{memberId}/reviews")
-    public ApiResponse<MemberResponseDTO.MemberReviewPreviewListDTO> getMemberReviewList(@PathVariable(name = "memberId") Long memberID){
+    @Operation(summary = "특정 사용자의 리뷰 목록 조회 API", description = "특장 사용자의 리뷰 목록을 조회하는 API이며, 페이징을 포함합니다. 페이지는 10개로 구분되며, 첫 페이지는 1로 설정되어 있습니다. query String으로 page 번호를 주시면 해당하는 페이지의 List가 조회됩니다.")
+    @ApiResponses({
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "사용자의 아이디, path variable입니당.")
+    })
+    public ApiResponse<MemberResponseDTO.MemberReviewPreviewListDTO> getMemberReviewList(@PathVariable(name = "memberId") Long memberId, @RequestParam(name="page") Integer page){
+        MemberCommandService.getMemberReviewList(memberId, page);
         return null;
     }
 
