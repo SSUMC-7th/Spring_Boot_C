4805345d43dcb397d091923973e7950f6181dd9c
isumin <leesumin0526@gmail.com>
2024-11-26 19:07:36 +0900

    [practice/#9-getStoreMissions] DTO 생성

--- src/main/java/umc/spring/web/dto/MissionResponseDTO.java
+++ src/main/java/umc/spring/web/dto/MissionResponseDTO.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MissionResponseDTO {
     @Builder
@@ -18,5 +20,28 @@ public class MissionResponseDTO {
         Integer status;
         LocalDateTime createdAt;
         LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StoreMissionPreViewListDTO {
+        List<StoreMissionPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StoreMissionPreViewDTO {
+        String missionSpec;
+        Integer reward;
+        LocalDate deadLine;
     }
 }
