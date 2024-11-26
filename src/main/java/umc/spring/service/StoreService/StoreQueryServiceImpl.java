0b7036db789d21a75500d61348bf9f0e5a865e3f
isumin <leesumin0526@gmail.com>
2024-11-26 14:36:21 +0900

    [practice/#9] 기능 구현 완료!2

--- src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -2,8 +2,12 @@ package umc.spring.service.StoreService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository.StoreRepository;
 
 import java.util.List;
@@ -14,6 +18,7 @@ import java.util.Optional;
 @Transactional
 public class StoreQueryServiceImpl implements StoreQueryService{
     private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
@@ -27,5 +32,14 @@ public class StoreQueryServiceImpl implements StoreQueryService{
         filteredStores.forEach(store -> System.out.println("Store: " + store));
 
         return filteredStores;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+
+        Store store = storeRepository.findById(StoreId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다. storeId: " + StoreId));
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
     }
 }
