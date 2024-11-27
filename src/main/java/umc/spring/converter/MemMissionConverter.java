0035f760e8c7e928c36431ceca07761fdb628c5c
pwyi-cupi <155076527+pwyi-cupi@users.noreply.github.com>
2024-11-27 20:02:11 +0900

    mission : 내가 진행중인 미션 목록 조회 API (#70)

--- src/main/java/umc/spring/converter/MemMissionConverter.java
+++ src/main/java/umc/spring/converter/MemMissionConverter.java
@@ -1,6 +1,7 @@
 package umc.spring.converter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
@@ -11,6 +12,8 @@ import umc.spring.web.dto.MemMissionRequestDTO;
 import umc.spring.web.dto.MemMissionResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -34,6 +37,27 @@ public class MemMissionConverter {
         return MemberMission.builder()
                 .member(member)
                 .mission(mission)
+                .build();
+    }
+
+    public static MemMissionResponseDTO.ChallengingMissionDTO toChallengingMissionDTO(MemberMission memMission) {
+        return MemMissionResponseDTO.ChallengingMissionDTO.builder()
+                .storeName(memMission.getMission().getStore().getName())
+                .missionSpec(memMission.getMission().getMissionSpec())
+                .build();
+    }
+
+    public static MemMissionResponseDTO.ChallengingMissionListDTO toChallengingMissionListDTO(Page<MemberMission> memMissionList) {
+        List<MemMissionResponseDTO.ChallengingMissionDTO> memMissionDTOList = memMissionList.stream()
+                .map(MemMissionConverter::toChallengingMissionDTO).collect(Collectors.toList());
+
+        return MemMissionResponseDTO.ChallengingMissionListDTO.builder()
+                .isFirst(memMissionList.isFirst())
+                .isLast(memMissionList.isLast())
+                .totalPage(memMissionList.getTotalPages())
+                .totalElements(memMissionList.getTotalElements())
+                .listSize(memMissionDTOList.size())
+                .reviewList(memMissionDTOList)
                 .build();
     }
 }
